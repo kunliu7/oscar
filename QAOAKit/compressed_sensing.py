e54280f3292c272c9ebab7ba75d245f9fa5e1ddc
kunliu7 <952235037@qq.com>
2022-07-25 19:07:18 -0400

    feat: cs OK

---
+++ QAOAKit/compressed_sensing.py
@@ -0,0 +1,417 @@
+from typing import List
+import networkx as nx
+import numpy as np
+import pandas as pd
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute
+from qiskit import Aer
+import qiskit
+from qiskit.providers.aer import AerSimulator
+from functools import partial
+from pathlib import Path
+import copy
+import timeit
+import sys, os
+from scipy.fftpack import dct, diff, idct
+from scipy.optimize import minimize
+from sklearn import linear_model
+
+from qiskit.providers.aer.noise import NoiseModel
+import concurrent.futures
+from mitiq import zne, Observable, PauliString
+from mitiq.interface.mitiq_qiskit.qiskit_utils import (
+    execute,
+    execute_with_noise,
+    execute_with_shots_and_noise
+)
+
+from mitiq.interface import convert_to_mitiq
+
+from qiskit.quantum_info import Statevector
+from sympy import beta
+
+from .qaoa import get_maxcut_qaoa_circuit
+from .utils import (
+    noisy_qaoa_maxcut_energy,
+    angles_from_qiskit_format,
+    maxcut_obj,
+    get_adjacency_matrix,
+    obj_from_statevector
+)
+from .noisy_params_optim import (
+    compute_expectation,
+    get_depolarizing_error_noise_model
+)
+
+# vis
+import numpy as np
+import matplotlib.pyplot as plt
+from random import sample
+
+
+def cosamp(phi, u, s, epsilon=1e-10, max_iter=1000):
+    """
+    Return an `s`-sparse approximation of the target signal
+    Input:
+        - phi, sampling matrix
+        - u, noisy sample vector
+        - s, sparsity
+    
+    cosamp function is available at https://github.com/avirmaux/CoSaMP
+    """
+    a = np.zeros(phi.shape[1])
+    v = u
+    it = 0 # count
+    halt = False
+    while not halt:
+        it += 1
+        
+        y = np.dot(np.transpose(phi), v)
+        omega = np.argsort(y)[-(2*s):] # large components
+        omega = np.union1d(omega, a.nonzero()[0]) # use set instead?
+        phiT = phi[:, omega]
+        b = np.zeros(phi.shape[1])
+        # Solve Least Square
+        b[omega], _, _, _ = np.linalg.lstsq(phiT, u)
+        
+        # Get new estimate
+        b[np.argsort(b)[:-s]] = 0
+        a = b
+        
+        # Halt criterion
+        v_old = v
+        v = u - np.dot(phi, a)
+
+        diff_old = np.linalg.norm(v - v_old)
+        halt = (diff_old  < epsilon) or \
+            np.linalg.norm(v) < epsilon or \
+            it > max_iter
+        
+        print(f'iter = {it}, {diff_old:.3f}\r', end='')
+    return a
+
+def L1_norm(x):
+    return np.linalg.norm(x,ord=1)
+
+def solve_by_l1_norm(Theta, y):
+    constr = ({'type': 'eq', 'fun': lambda x:  Theta @ x - y})
+    x0 = np.linalg.pinv(Theta) @ y 
+    res = minimize(L1_norm, x0, method='SLSQP',constraints=constr)
+    s = res.x
+    return s
+
+def recon_by_Lasso(Theta, y):
+    n = Theta.shape[1]
+    lasso = linear_model.Lasso(alpha=0.001)# here, we use lasso to minimize the L1 norm
+    # lasso.fit(Theta, y.reshape((M,)))
+    lasso.fit(Theta, y)
+    # Plotting the reconstructed coefficients and the signal
+    # Creates the fourier transform that will most minimize l1 norm 
+    recons = idct(lasso.coef_.reshape((n, 1)), axis=0)
+    return recons + lasso.intercept_
+
+
+def recon_by_CoSaMP(y, Psi, perm, sparsity):
+    # n = x.shape[0]
+    # xt = np.fft.fft(x) # Fourier transformed signal
+    # PSD = xt * np.conj(xt) / n # Power spectral density
+    # p = n // 2 # num. random samples, p = n/32
+    # perm = np.floor(np.random.rand(p) * n).astype(int)
+    # y = x[perm]
+
+    # reconstruct
+    # Psi = dct(np.identity(n)) # Build Psi
+    Theta = Psi[perm,:]       # Measure rows of Psi
+    s = cosamp(Theta, y, sparsity, epsilon=1.e-10, max_iter=200) # CS via matching pursuit
+    # xrecon = idct(s) # reconstruct full signal
+    xrecon = np.fft.ifft(s)
+    return xrecon
+
+
+def _vis_y_and_x_recon(
+        C_opt, bound, var_opt,
+        full_range, var_range, x_recon, y,
+        xlabel, 
+        title,
+        save_path
+    ):
+    fig, ax = plt.subplots()
+
+    ax.hlines(y=C_opt, xmin=bound[0], xmax=bound[1], colors=['g'], label='C optimal')
+    ax.plot(var_opt, C_opt, marker="o", color='red', markersize=5, label='optimal point')
+    # print(unmitis)
+    # ax.plot(full_range, unmitis_recon, marker="o", markersize=2, label="unmitigated, recon")
+    ax.scatter(full_range, x_recon, marker="o", s=2, label="unmitigated, recon")
+    ax.scatter(var_range, y, marker="x", label="unmitigated, sample", c='red')
+    # ax.scatter(full_range, unmitis_recon, marker="o", s=2, label="unmitigated, recon")
+    
+    ax.set_ylabel('energy or cost value')
+    ax.set_xlabel(xlabel)
+    # ax.set_xlim(full_range[0], full_range[4096])
+    ax.set_ylim(-1, C_opt + 2)
+    ax.set_title(title)
+    ax.legend()
+
+    fig.savefig(save_path)
+    plt.close('all')
+
+
+def _vis_miti_and_unmiti_recon(
+        C_opt, bound, var_opt,
+        full_range,
+        mitis_recon,
+        unmitis_recon,
+        xlabel, 
+        title,
+        save_path
+    ):
+    fig, ax = plt.subplots()
+
+    ax.hlines(y=C_opt, xmin=bound[0], xmax=bound[1], colors=['g'], label='C optimal')
+    ax.plot(var_opt, C_opt, marker="o", color='b', markersize=5, label='optimal point')
+    # print(unmitis)
+    ax.scatter(full_range, mitis_recon, marker="o", s=2, label="mitigated, recon")
+    ax.scatter(full_range, unmitis_recon, marker="x", s=2, label="unmitigated, recon")
+    
+    ax.set_ylabel('energy or cost value')
+    ax.set_xlabel(xlabel)
+    # ax.set_xlim(full_range[0], full_range[4096])
+    ax.set_ylim(-1, C_opt + 2)
+    ax.set_title(title)
+    ax.legend()
+
+    fig.savefig(save_path)
+    plt.close('all')
+
+    
+
+
+def CS_and_one_landscape_and_cnt_optima_and_mitiq_and_one_variable(
+        G: nx.Graph,
+        p: int,
+        figdir: str,
+        var_idx: int, # p==2, 0-3 -> beta0, beta1, gamma0, gamma1
+        beta_opt: np.array, # converted
+        gamma_opt: np.array, # converted
+        noise_model: NoiseModel,
+        params_path: list,
+        C_opt: float,
+        sampling_frac: float
+    ):
+    # ! Convention: First beta, Last gamma
+
+
+    # ! executor that mitiq wants. only QuantumCircuit is needed
+    # ! get_statevector() needs qc.save_state(),
+    # and thus qc.qasm() could not be called
+    # and thus could not be transformed to mitiq.circuit
+
+    # obj = partial(maxcut_obj, w=get_adjacency_matrix(G))
+    # def mitiq_executor_of_qaoa_maxcut_energy(qc) -> float:
+    #     backend = AerSimulator(method="statevector")
+    #     noise_model = get_depolarizing_error_noise_model(p1Q=0.001, p2Q=0.005) # ! base noise model
+    #     sv = backend.run(
+    #         qc,
+    #         noise_model=noise_model
+    #     ).result().get_statevector()
+    #     return obj_from_statevector(sv, obj, precomputed_energies=None)
+
+    def mitiq_executor_of_qaoa_maxcut_energy(qc) -> float:
+        backend = AerSimulator()
+        qc.measure_all()
+        # backend = 
+        # backend = Aer.get_backend('aer_simulator')
+        noise_model = get_depolarizing_error_noise_model(p1Q=0.001, p2Q=0.005) # ! base noise model
+        # noise_model = None
+        counts = backend.run(
+            qc,
+            shots=2048,
+            noise_model=noise_model
+        ).result().get_counts()
+        
+        expval = compute_expectation(counts, G)
+        return -expval
+
+    # for edge in G.edges:
+    #     obs = Observable(PauliString("I" * n)) #, PauliString("X", coeff=-1.75))
+    # obs = Observable(PauliString("I"), PauliString("X", coeff=-1.75))
+
+    # n = G.number_of_nodes()
+    # def mitiq_executor_of_qaoa_maxcut_energy(qc) -> float:
+    #     noise_model = get_depolarizing_error_noise_model(p1Q=0.001, p2Q=0.005) # ! base noise model 
+    #     return execute_with_shots_and_noise(qc, obs.matrix(), noise_model, shots=1024)
+        
+    assert var_idx >= 0 and var_idx < 2*p
+    
+    # beta first, gamma later
+    b_or_g = 'beta' if var_idx < p else 'gamma'
+    bounds = {'beta': [-np.pi/4, np.pi/4], 'gamma': [-np.pi, np.pi]}
+    bound = bounds[b_or_g]
+    bound_len = bound[1] - bound[0]
+
+    # hyper parameters
+    n_pts = 36     # num. of original points per unit == 4096, i.e. resolution rate = 1 / n
+    sparsity = 4 # n_samples // 4       # K-sparse of s per unit
+    n_samples = int(n_pts * sampling_frac) # 12  # num. of random samples per unit
+    window = np.array([1024, 1280]) / n_pts
+
+    # extend by bound_len
+    n_pts = np.floor(n_pts * bound_len).astype(int)
+    # n_samples = np.floor(sparsity * np.log(n_pts / sparsity)).astype(int)
+    n_samples = np.floor(n_samples * bound_len).astype(int)
+    # sparsity = np.floor(sparsity * bound_len).astype(int)
+    # window = np.floor(window * bound_len).astype(int)
+
+    print(f'n_pts={n_pts}, n_samples={n_samples}')
+
+    # return
+
+    # sample P points from N randomly
+    perm = np.floor(np.random.rand(n_samples) * n_pts).astype(int)
+
+    full_range = np.linspace(bound[0], bound[1], n_pts)
+    # full_range = np.linspace(0, 1, n_pts)
+    var_range = full_range[perm]
+
+    # labels
+    _BETA_GAMMA_OPT = np.hstack([beta_opt, gamma_opt])
+    _X_Y_LABELS = [f'beta{i}' for i in range(p)]
+    _X_Y_LABELS.extend([f'gamma{i}' for i in range(p)])
+
+    mitis = []
+    unmitis = []
+
+    for v in var_range:
+        tmp_vars = _BETA_GAMMA_OPT.copy()
+        tmp_vars[var_idx] = v
+
+        circuit = get_maxcut_qaoa_circuit(
+            G, beta=tmp_vars[:p], gamma=tmp_vars[p:],
+            transpile_to_basis=True, save_state=False)
+
+        unmiti = mitiq_executor_of_qaoa_maxcut_energy(circuit.copy())
+        miti = zne.execute_with_zne(
+            circuit=circuit.copy(),
+            executor=mitiq_executor_of_qaoa_maxcut_energy,
+        )
+
+        mitis.append(miti)
+        unmitis.append(unmiti)
+
+    # x = np.cos(2 * 97 * np.pi * full_range) + np.cos(2 * 777 * np.pi * full_range)
+    # x = np.cos(2 * np.pi * full_range) # + np.cos(2 * np.pi * full_range)
+    # mitis = x[perm]
+    # unmitis = x[perm]
+    mitis = np.array(mitis)
+    unmitis = np.array(unmitis)
+
+    Psi = dct(np.identity(n_pts)) # Build Psi
+    
+    # unmitis_recon = recon(unmitis, Psi, perm, sparsity)
+    # mitis_recon = recon(mitis, Psi, perm, sparsity)
+    
+    # print('start: solve l1 norm')
+    unmitis_recon = recon_by_Lasso(Psi[perm, :], unmitis)
+    mitis_recon = recon_by_Lasso(Psi[perm, :], mitis)
+    # print('end: solve l1 norm')
+    
+    # ----- count optima
+
+    max_unmiti = unmitis.max()
+    improved_n_optima = np.sum(
+        # np.isclose(unmitis_recon, C_opt, atol=C_opt-max_miti) == True
+        mitis > max_unmiti
+    )
+    
+    max_unmiti_recon = unmitis_recon.max()
+    improved_n_optima_recon = np.sum(
+        # np.isclose(mitis_recon, C_opt, atol=abs(C_opt-max_unmiti_recon)) == True
+        mitis_recon > max_unmiti_recon
+    )
+
+    print('improved_n_optima, recon', improved_n_optima, improved_n_optima_recon)
+
+    # =============== vis unmiti ===============
+
+    _vis_y_and_x_recon(
+        C_opt=C_opt, bound=bound, var_opt=_BETA_GAMMA_OPT[var_idx],
+        full_range=full_range, var_range=var_range,
+        x_recon=mitis_recon, y=mitis,
+        xlabel=_X_Y_LABELS[var_idx],
+        title=f'QAOA energy, nQ{G.number_of_nodes()}, recon miti',
+        save_path=f'{figdir}/zne_varIdx={var_idx}_mitis_recon.png'
+    )
+    
+    _vis_y_and_x_recon(
+        C_opt=C_opt, bound=bound, var_opt=_BETA_GAMMA_OPT[var_idx],
+        full_range=full_range, var_range=var_range,
+        x_recon=unmitis_recon, y=unmitis,
+        xlabel=_X_Y_LABELS[var_idx],
+        title=f'QAOA energy, nQ{G.number_of_nodes()}, recon unmiti',
+        save_path=f'{figdir}/zne_varIdx={var_idx}_unmitis_recon.png'
+    )
+
+    _vis_miti_and_unmiti_recon(
+        C_opt=C_opt, bound=bound, var_opt=_BETA_GAMMA_OPT[var_idx],
+        full_range=full_range,
+        mitis_recon=mitis_recon,
+        unmitis_recon=unmitis_recon,
+        xlabel=_X_Y_LABELS[var_idx],
+        title=f'QAOA energy, nQ{G.number_of_nodes()}, reconstructed unmiti and miti',
+        save_path=f'{figdir}/zne_varIdx={var_idx}_unmitis_recon_and_mitis_recon.png'
+    )
+
+    np.savez_compressed(f"{figdir}/varIdx{var_idx}",
+        n_pts=n_pts, n_samples=n_samples, sparsity=sparsity,
+        mitis=mitis, unmitis=unmitis,
+        unmitis_recon=unmitis_recon, mitis_recon=mitis_recon,
+        perm=perm,
+        improved_n_optima=improved_n_optima,
+        improved_n_optima_recon=improved_n_optima_recon,
+        C_opt=C_opt)
+
+    return
+
+
+def multi_landscapes_and_cnt_optima_and_mitiq_and_MP_and_one_variable_and_CS(
+        G: nx.Graph, 
+        p: int,
+        figdir: str,
+        beta_opt: np.array, # converted
+        gamma_opt: np.array, # converted
+        noise_model: NoiseModel,
+        params_path: list,
+        C_opt: float,
+        executor: concurrent.futures.ProcessPoolExecutor
+    ):
+
+    if not os.path.exists(figdir):
+        os.makedirs(figdir)
+
+    params = []
+    for i in range(2*p):
+        params.append((
+            G.copy(),
+            p,
+            figdir,
+            i,
+            beta_opt.copy(),
+            gamma_opt.copy(),
+            noise_model.copy() if noise_model else None,
+            params_path.copy(),
+            C_opt
+        ))
+    
+    print('choose 10 randomly:', len(params))
+
+    print('start MP')
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+        # n_optima_list = executor.map(
+        #     lambda x: vis_landscape_heatmap_multi_p_and_count_optima(*x), params, chunksize=16)
+    for param in params:
+        future = executor.submit(
+            CS_and_one_landscape_and_cnt_optima_and_mitiq_and_one_variable,
+            *param
+        )
+        # print(future.result())
+        
+    return [], []
---
+++ cs.py
@@ -0,0 +1,244 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+import concurrent.futures
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute
+from qiskit.algorithms.optimizers import (
+    ADAM,
+    AQGD,
+    CG,
+    COBYLA,
+    L_BFGS_B,
+    GSLS,
+    GradientDescent,
+    NELDER_MEAD,
+    NFT,
+    P_BFGS,
+    POWELL,
+    SLSQP,
+    SPSA,
+    QNSPSA,
+    TNC,
+    SciPyOptimizer
+)
+from qiskit.algorithms import VQE, NumPyMinimumEigensolver, QAOA
+from qiskit.utils import QuantumInstance, algorithm_globals
+# from qiskit_optimization.algorithms import (
+#     MinimumEigenOptimizer,
+#     RecursiveMinimumEigenOptimizer,
+#     SolutionSample,
+#     OptimizationResultStatus,
+#     WarmStartQAOAOptimizer
+# )
+from qiskit import Aer
+import qiskit
+from qiskit.providers.aer import AerSimulator
+from functools import partial
+from pathlib import Path
+import copy
+from itertools import groupby
+import timeit
+import sys, os
+from scipy.optimize import minimize
+
+from qiskit.quantum_info import Statevector
+# from QAOAKit import vis
+
+sys.path.append('..')
+from QAOAKit.noisy_params_optim import (
+    get_pauli_error_noise_model,
+    optimize_under_noise,
+    get_depolarizing_error_noise_model,
+    compute_expectation
+)
+
+from QAOAKit.vis import(
+    vis_landscape,
+    vis_landscape_heatmap,
+    vis_landscape_heatmap_multi_p,
+    vis_landscape_multi_p,
+    vis_landscape_multi_p_and_and_count_optima,
+    vis_landscape_multi_p_and_and_count_optima_MP,
+    vis_multi_landscape_and_count_optima_and_mitiq_MP,
+    vis_multi_landscapes_and_count_optima_and_mitiq_MP_and_one_variable,
+)
+
+from QAOAKit import (
+    opt_angles_for_graph,
+    get_fixed_angles,
+    get_graph_id,
+    get_graph_from_id,
+    angles_to_qaoa_format,
+    beta_to_qaoa_format,
+    gamma_to_qaoa_format,
+    angles_to_qiskit_format,
+    angles_to_qtensor_format,
+    get_3_reg_dataset_table,
+    get_3_reg_dataset_table_row,
+    get_full_qaoa_dataset_table_row,
+    get_full_qaoa_dataset_table,
+    get_fixed_angle_dataset_table,
+    get_fixed_angle_dataset_table_row,
+    qaoa_maxcut_energy,
+    noisy_qaoa_maxcut_energy,
+    angles_from_qiskit_format,
+
+)
+from QAOAKit.utils import (
+    beta_shift_sector,
+    gamma_shift_sector,
+    get_curr_formatted_timestamp,
+    load_partial_qaoa_dataset_table,
+    obj_from_statevector,
+    precompute_energies,
+    maxcut_obj,
+    isomorphic,
+    load_weights_into_dataframe,
+    load_weighted_results_into_dataframe,
+    get_adjacency_matrix,
+    brute_force,
+    get_pynauty_certificate,
+    get_full_weighted_qaoa_dataset_table,
+    save_partial_qaoa_dataset_table
+)
+
+from QAOAKit.classical import thompson_parekh_marwaha
+from QAOAKit.qaoa import get_maxcut_qaoa_circuit
+from QAOAKit.qiskit_interface import (
+    get_maxcut_qaoa_qiskit_circuit,
+    goemans_williamson,
+    get_maxcut_qaoa_qiskit_circuit_unbinded_parameters
+)
+from QAOAKit.examples_utils import get_20_node_erdos_renyi_graphs
+from QAOAKit.parameter_optimization import get_median_pre_trained_kde
+from QAOAKit.compressed_sensing import (
+    CS_and_one_landscape_and_cnt_optima_and_mitiq_and_one_variable,
+    multi_landscapes_and_cnt_optima_and_mitiq_and_MP_and_one_variable_and_CS
+)
+
+
+# from qiskit_optimization import QuadraticProgram
+from qiskit.algorithms.minimum_eigen_solvers.qaoa import QAOAAnsatz
+
+test_utils_folder = Path(__file__).parent
+
+
+def count_optima_of_fixed_angles_3reg_graphs_one_variable_CS():
+    """Count optima for regular 3 graphs with p>=3.
+
+    In QAOAKit (or more specifically, https://github.com/danlkv/fixed-angle-QAOA),
+    use fixed angles to solve 3 regular graphs.
+    These approximation ratios are well-bounded, so we take them as results.
+    """
+
+    MAX_NUM_GRAPHS_PER_NUM_QUBITS = 10
+    count_rst_df = pd.DataFrame(
+        columns=[
+            'row_id', 'G', 'pynauty_cert',
+            'n_qubits', 'p',
+            'miti_n_opt_list',
+            'unmiti_n_opt_list',
+            'has_opt',
+            'C_opt'
+        ]
+    )
+    
+    reg3_dataset_table = get_3_reg_dataset_table()
+    print("read 3 reg dataset OK")
+    print("use fixed angles to calculate n_optima")
+    signature = get_curr_formatted_timestamp()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for n_qubits in range(12, 13, 2): # [1, 16]
+        # for n_qubits in range(4, 5): # [1, 16]
+            for p in range(2, 3): # [1, 11]
+                start_time = time.time()
+                df = reg3_dataset_table.reset_index()
+                df = df[(df["n"] == n_qubits) & (df["p_max"] == p)]
+                df = df.iloc[7: MAX_NUM_GRAPHS_PER_NUM_QUBITS]
+                # df = df.iloc[0: 1]
+                # df = df.iloc[MAX_NUM_GRAPHS_PER_NUM_QUBITS:2*MAX_NUM_GRAPHS_PER_NUM_QUBITS]
+                
+                print(f"n_qubits={n_qubits}")
+                print(f"num of graphs: {len(df)}")
+                print(f"p={p}")
+                for row_id, row in df.iterrows():
+                    
+                    print(f"handling {row_id}")
+                    angles = angles_to_qaoa_format(get_fixed_angles(d=3, p=p))
+
+                    # print(row["beta"])
+                    # print(row["gamma"])
+                    # print(angles)
+                    # print(row["p_max"])
+                    # print(row["C_opt"], row["C_{true opt}"], row["C_fixed"])
+
+                    C_opt = row["C_fixed"]
+                    print("C_fixed", C_opt)
+                    G = row["G"]
+
+                    figdir = f'figs/cnt_opt_miti/{signature}/G{row_id}_nQ{n_qubits}_p{p}'
+                    
+                    if not os.path.exists(figdir):
+                        os.makedirs(figdir)
+
+                    nx.draw_networkx(G)
+                    plt.title(f"")
+                    plt.savefig(f"{figdir}/G{row_id}.png")
+                    plt.cla()
+
+                    # n_optima_list = vis_landscape_multi_p_and_and_count_optima(
+                    miti_n_opt_list, unmiti_n_opt_list = \
+                        multi_landscapes_and_cnt_optima_and_mitiq_and_MP_and_one_variable_and_CS(
+                    # miti_n_opt_list, unmiti_n_opt_list = vis_multi_landscapes_and_count_optima_and_mitiq_MP_and_one_variable(
+                            G=G,
+                            p=p,
+                            figdir=figdir, 
+                            # beta_opt=beta_to_qaoa_format(angles["beta"]),
+                            # gamma_opt=gamma_to_qaoa_format(angles["gamma"]),
+                            beta_opt=angles["beta"],
+                            gamma_opt=angles["gamma"],
+                            noise_model=None,
+                            params_path=[],
+                            C_opt=C_opt,
+                            executor=executor
+                    )
+
+                    print('miti_n_opt_list', miti_n_opt_list)
+                    print('unmiti_n_opt_list', unmiti_n_opt_list)
+
+                    # count_rst_df = count_rst_df.append({
+                    #     'row_id': row_id,
+                    #     'G': G,
+                    #     'pynauty_cert': row['pynauty_cert'],
+                    #     'n_qubits': n_qubits,
+                    #     'p': p,
+                    #     'miti_n_opt_list': miti_n_opt_list,
+                    #     'unmiti_n_opt_list': unmiti_n_opt_list,
+                    #     'has_opt': False,
+                    #     'C_opt': C_opt,
+                    # }, ignore_index=True)
+
+                    # print(count_rst_df)
+                    # count_rst_df.to_pickle(f"cnt_opt_miti_df/{signature}_cnt_opt_fixed_angles.p")
+                    print(" ================ ")
+
+                end_time = time.time()
+                print(f"for p={p}, nQ={n_qubits}, it takes {end_time-start_time} s")
+
+    return
+
+
+if __name__ == "__main__":
+    # test_qiskit_qaoa_circuit()
+    # test_noisy_qaoa_maxcut_energy()
+    # test_optimization_method()
+    # test_qiskit_qaoa_circuit_optimization()
+    # test_removing_edges()
+    # count_optimals()
+    # count_optima_of_fixed_angles_3reg_graphs()
+    # count_optima_of_specific_3reg_graph(nQ_range=[4,17])
+    # print_graphs()
+    # count_optima_of_fixed_angles_3reg_graphs()
+    count_optima_of_fixed_angles_3reg_graphs_one_variable_CS()
