ee5031728dc3efad45883b4cf1f2cd13a965c734
kunliu7 <952235037@qq.com>
2023-03-11 13:38:16 -0500

    refactor: format

--- oscar/optimizer_wrapper.py
+++ oscar/optimizer_wrapper.py
@@ -16,7 +16,8 @@ from functools import partial
 from pathlib import Path
 import copy
 import timeit
-import sys, os
+import sys
+import os
 from scipy.fftpack import dct, diff, idct
 from scipy.optimize import minimize
 from sklearn import linear_model
@@ -68,18 +69,22 @@ from qiskit.algorithms.optimizers import (
 
 from qiskit.algorithms.optimizers.optimizer import POINT
 
+
 def get_numerical_derivative(fun: Optional[Callable[[POINT], POINT]], eps=1e-10):
     jac = Optimizer.wrap_function(Optimizer.gradient_num_diff, (fun, eps))
     return jac
 
-def wrap_qiskit_optimizer_to_landscape_optimizer(QiskitOptimizer):
-    class LandscapeOptimizer(QiskitOptimizer):
-    
-        """Override some methods based on existing Optimizers.
-        Since we have existing landscape, we do not need to actually execute the circuit.
 
-        Dynamic inheritance: https://stackoverflow.com/a/21060094/13392267
-        """
+def wrap_qiskit_optimizer_to_landscape_optimizer(QiskitOptimizer):
+    """Override `minimize` of existing Qiskit Optimizers. 
+
+    Since we have existing landscape, we do not need to actually execute the circuit simulation.
+    Based on the `fun_type` parameter, we can choose the implementation of the `f` function.
+
+    We use the dynamic inheritance (https://stackoverflow.com/a/21060094/13392267) to
+    do the override.
+    """
+    class LandscapeOptimizer(QiskitOptimizer):
         def __init__(self, bounds=None, landscape=None, fun_type='FUN', fun=None, **kwargs) -> None:
             # https://blog.csdn.net/sunny_happy08/article/details/82588749
             print("kwargs", kwargs)
@@ -90,38 +95,58 @@ def wrap_qiskit_optimizer_to_landscape_optimizer(QiskitOptimizer):
             if self.fun_type == 'FUN':
                 self.fun = fun
                 return
-            
+
             self.bounds = bounds
             self.landscape = landscape
             self.landscape_shape = np.array(landscape.shape)
 
             # https://numpy.org/doc/stable/reference/generated/numpy.apply_along_axis.html
-            bound_lens = np.apply_along_axis(lambda bound: bound[1] - bound[0], axis=1, arr=bounds)
+            bound_lens = np.apply_along_axis(
+                lambda bound: bound[1] - bound[0], axis=1, arr=bounds)
             print('bound_lens', bound_lens)
-            self.grid_lens = bound_lens / self.landscape_shape # element-wise
+            self.grid_lens = bound_lens / self.landscape_shape  # element-wise
             print("grid_lens", self.grid_lens)
 
+        def minimize(self,
+                     fun: Callable[[POINT], float],
+                     x0: POINT,
+                     jac: Optional[Callable[[POINT], POINT]] = None,
+                     bounds: Optional[List[Tuple[float, float]]] = None) -> OptimizerResult:
+            """Override existing optimizer's minimize function.
 
-        # def minimize(self, 
-        #     fun: Callable[[POINT], float], 
-        #     x0: POINT, 
-        #     jac: Optional[Callable[[POINT], POINT]] = None,
-        #     bounds: Optional[List[Tuple[float, float]]] = None):
-        #     return super().minimize(fun, x0, jac, bounds)
+            Notice that the parameter `fun` is not used here.
+            We use our own `fun` function based on the `fun_type` parameter.
+            """
+            if self.fun_type == 'INTERPOLATE':
+                res = super().minimize(self.approximate_fun_value, x0, jac, bounds)
+            if self.fun_type == 'INTERPOLATE_QISKIT':
+                res = super().minimize(self.approximate_fun_value_qiskit, x0, jac, bounds)
+            elif self.fun_type == 'FUN':
+                res = super().minimize(self._fun, x0, jac, bounds)
+            else:
+                res = super().minimize(self.query_fun_value_from_landscape, x0, jac, bounds)
+            print('res', res)
+            print(jac)
+            return res
 
-        # def qiskit_format_to_qaoa_format_arr(self, x: POINT):
         @staticmethod
         def qiskit_format_to_qaoa_format_arr(x: POINT):
+            """Convert the qiskit format to qaoa format.
+
+            These are different formats used by different circuit implementations. 
+            See the `angles_from_qiskit_format`
+            and `angles_to_qaoa_format` functions for more details.
+
+            """
             angles = angles_from_qiskit_format(x)
             angles = angles_to_qaoa_format(angles)
 
             x = np.concatenate([angles['gamma'], angles['beta']])
             return x
 
-
+        @DeprecationWarning
         def approximate_fun_value(self, x: POINT) -> float:
             x = self.qiskit_format_to_qaoa_format_arr(x)
-
             x = shift_parameters(x, self.bounds)
             val = approximate_fun_value_by_2D_interpolation(
                 x=x,
@@ -130,10 +155,8 @@ def wrap_qiskit_optimizer_to_landscape_optimizer(QiskitOptimizer):
             )
 
             self.params_path.append(x)
-            print("appro", val)
             self.vals.append(val[0])
             return val[0]
-        
 
         def approximate_fun_value_qiskit(self, x: POINT) -> float:
             """
@@ -149,10 +172,10 @@ def wrap_qiskit_optimizer_to_landscape_optimizer(QiskitOptimizer):
             )
 
             self.params_path.append(x)
-            # print("appro", val)
             self.vals.append(val[0])
             return val[0]
 
+        @DeprecationWarning
         def _fun(self, x: POINT) -> float:
             x = self.qiskit_format_to_qaoa_format_arr(x)
             # angles = angles_from_qiskit_format(x)
@@ -162,16 +185,16 @@ def wrap_qiskit_optimizer_to_landscape_optimizer(QiskitOptimizer):
             self.params_path.append(x)
             return val
 
-
-        def query_fun_value_from_landscape(self, x: POINT) -> float:        
+        def query_fun_value_from_landscape(self, x: POINT) -> float:
             angles = angles_from_qiskit_format(x)
             angles = angles_to_qaoa_format(angles)
 
             x = np.concatenate([angles['gamma'], angles['beta']])
-            print('transformed x', x)
+            # print('transformed x', x)
             # assert x.shape == self.grid_shapes.shape
             # print(type(bounds))
-            relative_indices = np.around((x - self.bounds[:, 0]) / self.grid_lens).astype(int)
+            relative_indices = np.around(
+                (x - self.bounds[:, 0]) / self.grid_lens).astype(int)
             normalized_indices = relative_indices.copy()
 
             for axis, size in enumerate(self.landscape_shape):
@@ -180,77 +203,16 @@ def wrap_qiskit_optimizer_to_landscape_optimizer(QiskitOptimizer):
                 if relative_index >= size:
                     normalized_indices[axis] = relative_index % size
                 elif relative_index < 0:
-                    normalized_indices[axis] = relative_index + ((-relative_index - 1) // size + 1) * size
+                    normalized_indices[axis] = relative_index + \
+                        ((-relative_index - 1) // size + 1) * size
 
                 if normalized_indices[axis] < 0 or normalized_indices[axis] >= size:
-                    print(axis, size, relative_index, normalized_indices[axis])
+                    print(axis, size, relative_index,
+                          normalized_indices[axis])
                     assert False
 
-            # print(normalized_indices)
-            # print(self.landscape_shape)
-            # obj = *(list(normalized_indices))
             approximate_point = self.landscape[tuple(normalized_indices)]
-
-            # print(approximate_point)
-            self.params_path.append(x) # Qiskit format
+            self.params_path.append(x)  # Qiskit format
             return approximate_point
 
-
-        def minimize(self, 
-            fun: Callable[[POINT], float], 
-            x0: POINT, 
-            jac: Optional[Callable[[POINT], POINT]] = None,
-            bounds: Optional[List[Tuple[float, float]]] = None) -> OptimizerResult:
-            """Override existing optimizer's minimize function
-
-            """
-            def query_fun_value_from_landscape(x: POINT) -> float:
-                angles = angles_from_qiskit_format(x)
-                angles = angles_to_qaoa_format(angles)
-
-                x = np.concatenate([angles['gamma'], angles['beta']])
-                # print('transformed x', x)
-                # assert x.shape == self.grid_shapes.shape
-                # print(type(bounds))
-                relative_indices = np.around((x - self.bounds[:, 0]) / self.grid_lens).astype(int)
-                normalized_indices = relative_indices.copy()
-
-                for axis, size in enumerate(self.landscape_shape):
-                    # print(axis, size)
-                    relative_index = relative_indices[axis]
-                    if relative_index >= size:
-                        normalized_indices[axis] = relative_index % size
-                    elif relative_index < 0:
-                        normalized_indices[axis] = relative_index + ((-relative_index - 1) // size + 1) * size
-
-                    if normalized_indices[axis] < 0 or normalized_indices[axis] >= size:
-                        print(axis, size, relative_index, normalized_indices[axis])
-                        assert False
-
-                # print(normalized_indices)
-                # print(self.landscape_shape)
-                # obj = *(list(normalized_indices))
-                approximate_point = self.landscape[tuple(normalized_indices)]
-
-                # print(approximate_point)
-                self.params_path.append(x) # Qiskit format
-                return approximate_point
-
-            
-            if self.fun_type == 'INTERPOLATE':
-                res = super().minimize(self.approximate_fun_value, x0, jac, bounds)
-            if self.fun_type == 'INTERPOLATE_QISKIT':
-                res = super().minimize(self.approximate_fun_value_qiskit, x0, jac, bounds)
-            elif self.fun_type == 'FUN':
-                res = super().minimize(self._fun, x0, jac, bounds)
-            else:
-                # TODO replace query_fun_value_from_landscape by self.query_fun_value_from_landscape, and test
-                res = super().minimize(query_fun_value_from_landscape, x0, jac, bounds)
-            print('res', res)
-            print(jac)
-            return res
-
-
-
     return LandscapeOptimizer
-
