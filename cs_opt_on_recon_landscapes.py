92da24045c3065abef7a5fa6167e5200fb098c1a
kunliu7 <952235037@qq.com>
2023-03-11 20:35:44 -0500

    refactor: parser arguments

--- cs_opt_on_recon_landscapes.py
+++ cs_opt_on_recon_landscapes.py
@@ -3,20 +3,26 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--ns', type=int, nargs='+', help="Your aims, vis, opt", required=True)
-    parser.add_argument('-n', type=int, help="Your aims, vis, opt", required=True)
-    parser.add_argument('-p', type=int, help="Your aims, vis, opt", required=True)
-    # parser.add_argument('--method', type=str, help="Your aims, vis, opt", required=True)
-    parser.add_argument('--noise', type=str, help="Your aims, vis, opt", required=True)
-    parser.add_argument('--miti', type=str, help="Your aims, vis, opt", default=None)
-    parser.add_argument('--seed', type=int, help="Your aims, vis, opt", required=True)
-    parser.add_argument('--lr', type=float, help="Your aims, vis, opt", default=None)
-    parser.add_argument('--maxiter', type=int, help="Your aims, vis, opt", default=None)
-    parser.add_argument('--init_pt', type=float, nargs="+", help="[beta, gamma]", required=True)
+    parser.add_argument('-n', type=int, help="#qubits", required=True)
+    parser.add_argument('-p', type=int, help="#layers", required=True)
+    parser.add_argument('--noise', type=str,
+                        help="noise config", required=True)
+    parser.add_argument('--miti', type=str,
+                        help="mitigation config", default=None)
+    parser.add_argument('--seed', type=int,
+                        help="seed of instance", required=True)
+    parser.add_argument('--lr', type=float, help="learning rate", default=None)
+    parser.add_argument('--maxiter', type=int,
+                        help="max iteration of optimization", default=None)
+    parser.add_argument('--init_pt', type=float, nargs="+",
+                        help="[beta, gamma]", required=True)
     # parser.add_argument('--error', type=str, help="Your aims, vis, opt", required=True)
-    parser.add_argument('--check', action="store_true", help="Your aims, vis, opt", default=False)
-    parser.add_argument('--opt', type=str, help="Optimizer Name.", required=True)
-    parser.add_argument('--get_vals', help="Get values on the path.", action='store_true', default=False)
+    parser.add_argument('--check', action="store_true",
+                        help="check code", default=False)
+    parser.add_argument('--opt', type=str,
+                        help="optimizer Name.", required=True)
+    parser.add_argument('--get_vals', help="get values corresponding to parameters on the path",
+                        action='store_true', default=False)
     args = parser.parse_args()
 
     optimize_on_p1_reconstructed_landscape(
