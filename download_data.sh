383b76282117318f00f44ee4a47f62f2ac7e7f70
kunliu7 <952235037@qq.com>
2023-03-12 13:20:49 -0400

    feat: empty bash

---
+++ download_data.sh
@@ -0,0 +1,18 @@
+
+# TODO:
+
+# # Google's landscape, too big to upload
+# google_data/Google_Data/
+# google_data/Google\ Data.zip
+# google_data/Code_Patch.zip
+# google_data/Code_Patch/
+
+# # IBM data, too big to upload
+# figs/ibm/IBM_Exp_2048.zip
+# figs/ibm/IBM_Exp_2048/
+# figs/ibm/Experiments.zip
+# figs/ibm/Experiments/
+
+# # recon data, too big to upload
+# figs/grid_search_recon/
+# figs/grid_search/
